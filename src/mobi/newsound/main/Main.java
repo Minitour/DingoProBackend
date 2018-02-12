d0fce2b60a331a97110c6421003a91cc9008f231
Tony Zaitoun <tony.z.1711@gmail.com>
2018-02-12 20:36:56 +0200

    Added inital code + static config files

--- src/mobi/newsound/main/Main.java
+++ src/mobi/newsound/main/Main.java
@@ -1,8 +1,30 @@
 package mobi.newsound.main;
 
+import mobi.newsound.util.JSONTransformer;
+import mobi.newsound.util.RESTRoute;
+import org.apache.log4j.BasicConfigurator;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static mobi.newsound.util.Config.config;
+import static spark.Spark.post;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        BasicConfigurator.configure();
+
+        port(config.get("port").getAsInt());
+
+        //TODO: remove this later
+        get("/test","application/json",(request, response) -> {
+            response.header("Content-Type","application/json");
+            return "Hello World";
+        },new JSONTransformer());
+    }
+
+    static void make(String route, RESTRoute controller){
+        post(route, "application/json", controller,new JSONTransformer());
     }
 }
